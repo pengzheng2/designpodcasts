295a4efde9115d951d9d00ff49d9b32eca473864
“pengzheng2” <pzheng2@andrew.cmu.edu>
2018-11-30 19:36:14 -0500

    json

--- js/subpage.js
+++ js/subpage.js
@@ -2,7 +2,7 @@
 
 // Get data from a JSON file
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://raw.githubusercontent.com/pengzheng2/designpodcasts/master/data.json');
+ourRequest.open('GET', 'https://api.jsonbin.io/b/5c01d76190a73066ac184ab8');
 ourRequest.onload = function() {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
     // This is where we'll do something with the retrieved data
