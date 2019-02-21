32512bdb3489ab7b7bbf46d326a3eb4aafd02f36
pengzheng2 <43544799+pengzheng2@users.noreply.github.com>
2019-02-20 23:45:07 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // Get data from a JSON file
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://api.jsonbin.io/b/5c6e2b4b0177dd175173f991');
+ourRequest.open('GET', 'https://api.jsonbin.io/b/5c01d76190a73066ac184ab8/10');
 ourRequest.onload = function() {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
     // This is where we'll do something with the retrieved data
